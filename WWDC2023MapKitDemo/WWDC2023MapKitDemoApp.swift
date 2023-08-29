c3b0d7649b79de9662d33416775e202f65ad7b4d
Todd Boward <todd@professionalaerial.net>
2023-08-29 18:02:46 -0400

    Initial Commit

---
+++ WWDC2023MapKitDemo/WWDC2023MapKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC2023MapKitDemoApp.swift
+//  WWDC2023MapKitDemo
+//
+//  Created by Todd Boward on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WWDC2023MapKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
